7f27d79ae4a85af19996c9f9b8c80d35bab58df5
LucasUlegard <lucas.ulegard@gmail.com>
2023-10-01 10:55:46 +0200

    updated flask in req..txt

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ import io
 import pandas as pd
 
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+app = dash.Dash(__name__) #, suppress_callback_exceptions=True)
 server = app.server
 
 
